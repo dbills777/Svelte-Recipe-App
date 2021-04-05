3e0a3ca24c5c3c682e055dda9f869056b1b0ef85
erinhancock794 <erinhancock794@gmail.com>
2021-04-04 21:06:22 -0600

    add remove and complete items in shopping list

--- src/components/ShoppingList.svelte
+++ src/components/ShoppingList.svelte
@@ -1,55 +1,139 @@
 <script>
-    let newItem = '';
-    let quantity = '';
-    let shoppingList = [
-        {
-            ingredient: 'Apples',
-            quantity: {
-                unit: null,
-                amount: .5
-            },
-            status: false
-        },
-        {
-            ingredient: 'Peanut Butter',
-            quantity: {
-                unit: 'oz',
-                amount: 3
-            },
-            status: false
-        }
+      let items = [
+    { id: 1, name: "Apples", quantity: 2, done: false },
+    { id: 2, name: "Bananas", quantity: 4, done: true },
+    { id: 3, name: "Eggs", quantity: 4, done: false },
+    { id: 4, name: "Bread", quantity: 1, done: false }
+
+  ];
+  let name = "";
+  let quantity = "";
+
+  const addItem = () => {
+    items = [
+      ...items,
+      {
+        id: Math.random(),
+        name,
+        quantity,
+        done: false
+      }
     ];
+    name = "";
+    quantity = "";
+  };
 
-    function addToList() {
-        shoppingList = [...shoppingList, {ingredient: newItem, quantity, status: false}];
-        newItem = '';
-    }
+  const remove = item => {
+    items = items.filter(i => i !== item);
+  };
 
-    function removeFromList(index) {
-        shoppingList.splice(index, 1)
-        shoppingList = shoppingList
-    }
+//   const toggle = item => {
+//     item.done = !item.done;
+//     items = items;
+//   };
+    // let newItem = '';
+    // let quantity = '';
+    // let ingredient = '';
+    // let shoppingList = [
+    //     {
+    //         ingredient: 'Apples',
+    //         quantity: {
+    //             unit: null,
+    //             amount: .5
+    //         },
+    //         status: false
+    //     },
+    //     {
+    //         ingredient: 'Peanut Butter',
+    //         quantity: {
+    //             unit: 'oz',
+    //             amount: 3
+    //         },
+    //         status: false
+    //     }
+    // ];
+//     const toggle = item => {
+//     item.status = !item.status;
+//     items = items;
+//   };
+
+//     function addToList() {
+//         shoppingList = [...shoppingList, {ingredient: newItem, quantity, status: false}];
+//         newItem = '';
+//     }
+
+//     function removeFromList(item) {
+//         shoppingList.splice(index, 1)
+//         shoppingList = shoppingList
+
+//     }
 </script>
 
-<input bind:value={newItem} type='text' placeholder='add another ingredient'>
+<!-- <input bind:value={newItem} type='text' placeholder='add another ingredient'>
 <button on:click={addToList}>Add</button>
-<br/>
+<br/> -->
+<div>
+    <h1>Shopping List</h1>
+  
+    <form on:submit|preventDefault={addItem}>
+      <label for="name">Add an item</label>
+      <div class="inputs">
+      <input id="name" type="text" placeholder="name" bind:value={name} />
+      <input id="quantity" type="text" placeholder="quantity" bind:value={quantity} />
+    </div>
+      <button on:submit|preventDefault={addItem}
+      >Add </button>
 
-{#each shoppingList as item, index}
-    <input bind:checked={item.status} type='checkbox'>
-    {#if item.status} 
-        <span on:click={() => removeFromList(index)
-            }></span>
-    {:else}
-    <span class:checked={item.status} >{item.ingredient}</span>
-    <span on:click={() => removeFromList(index)}> X </span>
-    {/if}
-    <br/>
-{/each}
+    </form>
+  
+    <ul>
+      {#each items as item}
+        <li class:done={item.done}>
+            <input type="checkbox" bind:checked={item.done} />
+            <span>{item.name}</span>
+  
+    
+          <span id="quantity">{item.quantity}</span>
+
+          <span id="remove" on:click={() => remove(item)}>&times;</span>
+        </li>
+      {/each}
+    </ul>
+  </div>
+
 
 
 
 
 <style>
+form div.inputs{
+    /* color: red; */
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+}
+
+li {
+    list-style: none;
+    /* display: grid; */
+    /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+    /* justify-content: space-between; */
+}
+li #remove{
+    /* float: right; */
+    justify-items: flex-end;
+    border: none;
+    background: transparent;
+    padding: 0;
+    margin: 0;
+    color: #dc4f21;
+    font-size: 18px;
+    cursor: pointer;
+  }
+  li #quantity {
+      color:rgb(165, 165, 165);
+  }
+.done span {
+    opacity: 0.4;
+  }
 
 </style>
