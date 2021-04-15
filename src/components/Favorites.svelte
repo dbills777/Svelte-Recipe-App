685dd841d6a7eb029dfb6276333a6cc9bed21cfe
Logan Uibel <42973092+lbuibel@users.noreply.github.com>
2021-04-15 14:39:53 -0600

    formatted ingredient list, added option to add ingredient to grocery list

--- src/components/Favorites.svelte
+++ src/components/Favorites.svelte
@@ -11,10 +11,18 @@
     // used in recipe modal
     let selectedRecipe = {}
 
+    let groceryList = []
+
     //updates recipe when user clicks on title of recipe in card
-    function handleClick(recipe) {
+    function displayRecipe(recipe) {
         selectedRecipe = recipe;
     }
+
+    function addGrocery(item) {
+        groceryList.push(item)
+        console.log('grocery list: ', groceryList)
+    }
+
 
 </script>
 
@@ -25,24 +33,32 @@
     <div class="modal-card">
         <img class="modal-img" src="{selectedRecipe.image}" alt="anything">
         <div class=modal-info>
-            <h3>{selectedRecipe.name}</h3>
-            <p>Ingredients</p>
-            {#each selectedRecipe.ingredients as ingredient}
-            <!-- TODO -->
-            <!-- update ingredient name so it can be clickable - added to grocery list -->
-            <!-- add quantities / units -->
-            <p>{ingredient.name}</p>
-            {/each}
+            <h3 class="recipe-title">{selectedRecipe.name}</h3>
 
-            <p>Instructions</p>
-            <!-- TODO -->
-            <!-- formatting still needs work -->
-            {#each selectedRecipe.instructions as instruction, i}
-                <p>step {i+1}: {instruction}</p>
-            {/each}
+            <!-- TODO: add styling or confirmation that ingredient has be added to grocery list after click-->
+            <table>
+                <tr>
+                  <th>Ingredient</th>
+                  <th>Amount</th>
+                </tr>
+                {#each selectedRecipe.ingredients as ingredient}
+                <tr class="table-fade" on:click={() => addGrocery(ingredient)}>
+                  <td>{ingredient.name}</td>
+                  <td>{ingredient.quantity} {ingredient.unit}</td>
+                </tr>
+                {/each}
+              </table>
+
+            <div>
+                <h5>Instructions</h5>
+                {#each selectedRecipe.instructions as instruction, i}
+                    <p><b>step</b> {i+1}: {instruction}</p>
+                {/each}
+            </div>
         </div>
     </div>
 </Modal>
+
 
 
 <div class="favorites-main">
@@ -51,7 +67,7 @@
             <div class="recipe-card">
                 <img class="recipe-card-img" src="{recipe.image}" alt="anything">
                 <div class="recipe-card-info">
-                    <h5 on:click={handleClick(recipe), modal.show()}>{recipe.name}</h5>
+                    <h5 on:click={displayRecipe(recipe), modal.show()}>{recipe.name}</h5>
                     <div class="icon" on:click={() => onFavoriteClick(recipe)}>
                         <IoMdHeart />
                     </div>
@@ -83,7 +99,42 @@
     {/each}
 </div> -->
 
-<style>
+<style>   
+    h5 {
+        margin-top: 0;
+        margin-bottom: 5px;
+    }
+
+    table {
+        padding: .5rem;
+    }
+
+    .table-fade {
+        transition: ease-in-out .15s;
+    }
+
+    .table-fade:hover {
+        cursor: pointer;
+        background-color: rgb(224, 224, 224);
+    }
+    .table-fade:selected {
+        background-color: green;
+        color: white;
+    }
+
+    td {
+        font-size: 10px;
+        padding: 4px;
+    }
+    th {
+        padding-left: 0;
+    }
+
+    .recipe-title {
+        margin-top: 0;
+        margin-bottom: 1rem;
+    }
+
     .modal-card {
         display: flex;
     }
@@ -96,7 +147,8 @@
     }
     .modal-img {
         width: 40%;
-        height: 600px;
+        min-height: 600px;
+        height: 100%;
         object-fit: cover;
     }
 
