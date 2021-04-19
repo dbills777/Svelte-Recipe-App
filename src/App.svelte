120a880346940fd2fdd35c4685448ca23f3c93db
Derrick Bills <dbills777@gmail.com>
2021-04-19 12:06:21 -0600

    new nav design

--- src/App.svelte
+++ src/App.svelte
@@ -11,14 +11,36 @@ import NewRecipe from "./components/NewRecipe.svelte";
 </script>
 
 <main>
-
   <Router>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		<Link class="navbar-brand" to="/">Home</Link>
+
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+	 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		<ul class="navbar-nav mr-auto">
+			<li class="nav-item active">
+        <Link class="nav-link" to="/favorites">Favorites</Link>
+			</li>
+			<li class="nav-item">
+        <Link class="nav-link" to="/shoppinglist">ShoppingList</Link>
+			</li>
+			<li class="nav-item">
+      <Link class="nav-link" to="/newRecipe">New Recipe</Link>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+	</div>
+</nav>
     <header>
       <nav>
-		<Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
-        <Link to="/shoppinglist">ShoppingList</Link>
-        <Link to="/newRecipe">New Recipe</Link>
       </nav>
     </header>
 
