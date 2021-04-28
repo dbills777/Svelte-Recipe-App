347477bb05e5041287318eba037905f0527f0803
Derrick Bills <dbills777@gmail.com>
2021-04-28 17:34:43 -0600

    recipeStore.js added

---
+++ src/RecipeStore.js
@@ -0,0 +1,33 @@
+import { writable } from 'svelte/store'
+
+const groceryListStore = writable([
+    // initializing store with test data
+    {
+        "ingredients": [
+            {
+                "name": "test ingredient 1",
+                "unit": "cup",
+                "quantity": 3,
+                "type": "condiments",
+                "onList": false
+            },
+            {
+                "name": "store ingredient 2",
+                "unit": "tbsp",
+                "quantity": 3,
+                "type": "condiments",
+                "onList": false
+            },
+            {
+                "name": "store ingredient 3",
+                "unit": "tbsp",
+                "quantity": 3,
+                "type": "condiments",
+                "onList": false
+            }
+        ]
+
+    }
+])
+
+export default groceryListStore
