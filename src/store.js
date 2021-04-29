5ca32476574f9b2c31aa6093ff34be1016c6ea16
Logan Uibel <42973092+lbuibel@users.noreply.github.com>
2021-04-29 16:29:56 -0600

    setup store on both the favorites and homepage

--- src/store.js
+++ src/store.js
@@ -5,7 +5,7 @@ const groceryListStore = writable([
     {
         "ingredients": [
             {
-                "name": "test ingredient 1",
+                "name": "store ingredient 1",
                 "unit": "cup",
                 "quantity": 3,
                 "type": "condiments",
